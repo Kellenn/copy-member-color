f328605340eff42d053c280f7b75618703fd698a
Kellenn <70085049+Kellenn@users.noreply.github.com>
2022-02-16 23:09:06 -0700

    Fix context menu injections
    Closes #1

--- index.js
+++ index.js
@@ -1,36 +1,92 @@
 const { Plugin } = require("powercord/entities");
 const { React, getModule } = require("powercord/webpack");
-const { inject, uninject } = require("powercord/injector");
+const { uninject } = require("powercord/injector");
+const { injectContextMenu } = require("powercord/util");
 const { clipboard } = require("electron");
 
 module.exports = class CopyMemberColor extends Plugin {
   async startPlugin() {
     const Menu = await getModule(["MenuItem"]);
-    const GuildChannelUserContextMenu = await getModule((m) => m.default && m.default.displayName === "GuildChannelUserContextMenu");
-    const getMember = (await getModule(["getMember"]))["getMember"];
 
-    inject("copy-member-color", GuildChannelUserContextMenu, "default", (args, res) => {
-      const { guildId } = args[0];
-      const userId = args[0].user.id;
+    injectContextMenu("copy-member-color", "GuildChannelUserContextMenu", ([{ target }], res) => {
+      const parentElement = this.getParentElementFromChildElement(target);
+      const roleColorElementStyle = this.getUserColorElementStylesFromParentElement(parentElement);
 
-      const member = getMember(guildId, userId);
+      const rgb = this.getRgb(roleColorElementStyle?.getPropertyValue("color"));
 
-      // yo! we don't want members without colors yo!
-      if (member.colorString === null) {
+      if (!rgb) {
         return res;
       }
 
-      res.props.children.props.children.push(
+      const hex = this.getHexFromRgb(rgb);
+
+      res.props.children.push(
         React.createElement(Menu.MenuItem, {
           id: "copy-member-color",
           label: "Copy Color",
-          action: () => clipboard.writeText(member.colorString),
+          action: () => clipboard.writeText(hex),
         })
       );
 
       return res;
     });
-    GuildChannelUserContextMenu.default.displayName = "GuildChannelUserContextMenu";
+  }
+
+  getParentElementFromChildElement(element) {
+    /*
+    GuildChannelUserContextMenu contains two children elements,
+    avatar-6qzftW and content-1U25dZ, both with their own children.
+    Depending on where the user clicks, the element we get in our callback may change; hence,
+    we have to traverse the element tree until we get to the eldest parent element,
+    layout-1qmrhw. Then we can search its children's elements until we find our desired element.
+    */
+    while (element &&
+      !element?.classList?.contains("layout-1qmrhw") &&
+      !element?.classList?.contains("message-2CShn3")
+    ) {
+      element = element.parentNode;
+    }
+    return element;
+  }
+
+  getUserColorElementStylesFromParentElement(parentElement) {
+    if (!parentElement) {
+      return null;
+    }
+
+    return parentElement.getElementsByClassName(
+      "desaturateUserColors-1O-G89")[0]?.style;
+  }
+
+  getRgb(string) {
+    const rgbRegex = this.getRgbRegex();
+    const rgbRegexMatch = rgbRegex.exec(string);
+
+    if (!rgbRegexMatch) {
+      return null;
+    }
+
+    return {
+      red: parseInt(rgbRegexMatch[1]),
+      green: parseInt(rgbRegexMatch[2]),
+      blue: parseInt(rgbRegexMatch[3])
+    };
+  }
+
+  getRgbRegex() {
+    return /rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/;
+  }
+
+  getHexFromRgb(rgb) {
+    const firstByte = this.getHexFromRgbComponent(rgb.red);
+    const secondByte = this.getHexFromRgbComponent(rgb.green);
+    const thirdByte = this.getHexFromRgbComponent(rgb.blue);
+
+    return `#${firstByte}${secondByte}${thirdByte}`;
+  }
+
+  getHexFromRgbComponent(component) {
+    return parseInt(component).toString(16).padStart(2, '0');
   }
 
   pluginWillUnload() {
